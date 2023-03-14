7f9034c2c358b011625db973deffcd88abef22a1
Caio Mazur <caiomazur1@gmail.com>
2023-03-14 18:05:55 +0000

    pet creation pushes pet into user

--- routes/pets.routes.js
+++ routes/pets.routes.js
@@ -5,6 +5,24 @@ const mongoose = require("mongoose");
 const Pet = require("../models/Pet.model");
 const User = require("../models/User.model");
 
+const fileUploader = require("../config/cloudinary.config");
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("image"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+
+  res.json({ fileUrl: req.file.path });
+});
+
 //GET | Show all pets:
 router.get("/pets", async (req, res) => {
   try {
@@ -107,8 +125,8 @@ router.delete("/pets/edit/:id", async (req, res) => {
   }
 });
 
-//POST | Add a single pet:
-router.post("/pets/add", async (req, res) => {
+//POST | Add a single pet: // 1
+router.post("/pets/add", isAuthenticated, async (req, res) => {
   const data = ({
     name,
     species,
@@ -122,7 +140,8 @@ router.post("/pets/add", async (req, res) => {
     description,
     location,
   } = req.body);
-
+  const currentUser = req.payload._id; // 2
+  console.log(currentUser);
   try {
     // Check if requirements are provided as empty strings:
     if (
@@ -138,12 +157,25 @@ router.post("/pets/add", async (req, res) => {
       });
       return;
     }
-    const createdPet = await Pet.create(data);
-
-    res.status(201).json(createdPet);
+    const createdPet = await Pet.create(data); //3
+    console.log(createdPet);
+    const myUpdatedUser = await User.findByIdAndUpdate(
+      currentUser,
+      {
+        $push: { petsForAdoption: createdPet._id },
+      },
+      { new: true }
+    );
+    console.log(myUpdatedUser);
+    res.status(201).json(myUpdatedUser);
   } catch (error) {
     res.json(error);
   }
 });
 
 module.exports = router;
+
+/* const currentUser = req.payload._id;
+const myUser = await User.findById(currentUser)
+const myPetUpdated = await Pet.findByIdAndUpdate(createdPet._id, {$push: {interestedUsers: currentUser}}, {new: true})
+res.status(201).json(myPetUpdated); */
