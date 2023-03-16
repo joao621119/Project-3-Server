cf990efa2f5132620abca033f0712ec5d10b4158
Caio Mazur <caiomazur1@gmail.com>
2023-03-16 10:40:12 +0000

    second deployment attempt

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -11,7 +11,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    allowed_formats: ["jpg", "png", "svg", "webp"],
+    allowed_formats: ["jpg", "png", "svg", "webp", "jpeg"],
     folder: "movie-gallery", // The name of the folder in cloudinary
      resource_type: "raw", // => this is in case you want to upload other types of files, not just images
   }
